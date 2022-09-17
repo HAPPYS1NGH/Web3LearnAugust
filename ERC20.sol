9c46b1c89c89152d3e94c2f75fe043f83743fd34
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2022-09-17 13:37:58 +0530

    Create ERC20.sol

---
+++ ERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+contract HappyERC20 is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("HappyERC20", "HERC20") {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
